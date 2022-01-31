5c1da6a618bfa9050b87e409d7fcc6bb7d5b88d7
Aelie54 <klein.amelie1991@gmail.com>
2022-01-31 10:48:18 +0100

    premier enregistrement et config db

--- bootstrap.php
+++ bootstrap.php
@@ -23,10 +23,10 @@ $config->setAutoGenerateProxyClasses(1);
 // database configuration parameters
 $conn = array(
     'driver' => 'pdo_mysql',
-    'user'     => getenv('APP_USER'),
-    'password' => getenv('APP_PASS'),
-    'dbname'   => getenv('APP_BDD'),
-    'host'     => getenv('APP_HOST')
+    'user'     => getenv('106139_amelie'),
+    'password' => getenv('putmej-tizfo5-fegfiJ'),
+    'dbname'   => getenv('pawolanmwen_amelie'),
+    'host'     => getenv('mysql-pawolanmwen.alwaysdata.net')
 );
 
 // obtaining the entity manager
